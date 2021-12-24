754dcf487e7e6206609e41a6620013f112419fe9
Gabi <gabidunaevsky10@gmail.com>
2021-12-24 18:35:31 +0200

    creating class to save to file

---
+++ src/GraphToDisplay.py
@@ -0,0 +1,9 @@
+class GraphToDisplay:
+    def __init__(self):
+        self.Edges = []
+        self.Nodes = []
+
+class NodeToDisplay:
+    def __init__(self, pos, _id):
+        self.pos = pos
+        self.id = _id
