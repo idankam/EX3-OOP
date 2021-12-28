788da9223fc4d6b54642a0aa410719b31c4f0553
Idan_Kaminetsky <idankam98@gmail.com>
2021-12-28 18:20:05 +0200

    tests

---
+++ src/TestDiGraph.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+
+
+class TestDiGraph(TestCase):
+    def test_v_size(self):
+        self.fail()
+
+    def test_e_size(self):
+        self.fail()
+
+    def test_get_mc(self):
+        self.fail()
+
+    def test_add_edge(self):
+        self.fail()
+
+    def test_add_node(self):
+        self.fail()
+
+    def test_add_exist_node(self):
+        self.fail()
+
+    def test_remove_node(self):
+        self.fail()
+
+    def test_remove_edge(self):
+        self.fail()
+
+    def test_get_all_v(self):
+        self.fail()
+
+    def test_all_in_edges_of_node(self):
+        self.fail()
+
+    def test_all_out_edges_of_node(self):
+        self.fail()
+
+    def test_color_white(self):
+        self.fail()
+
+    def test_transpose(self):
+        self.fail()
---
+++ src/TestGraphAlgo.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+
+
+class TestGraphAlgo(TestCase):
+    def test_bfs(self):
+        self.fail()
+
+    def test_is_connected(self):
+        self.fail()
+
+    def test_load_from_json(self):
+        self.fail()
+
+    def test_save_to_json(self):
+        self.fail()
+
+    def test_nodes_edges_to_display(self):
+        self.fail()
+
+    def test_shortest_path(self):
+        self.fail()
+
+    def test_get_path(self):
+        self.fail()
+
+    def test_plot_graph(self):
+        self.fail()
+
+    def test_dijkstra(self):
+        self.fail()
+
+    def test_get_graph(self):
+        self.fail()
+
+    def test_tsp(self):
+        self.fail()
+
+    def test_nodes_list_to_int_lis(self):
+        self.fail()
+
+    def test_center_point(self):
+        self.fail()
