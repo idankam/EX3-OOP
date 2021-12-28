60d689561e85601273a400ca6e9c94ce680c6440
Gabi <gabidunaevsky10@gmail.com>
2021-12-28 19:50:11 +0200

    adding test to algo

--- src/TestGraphAlgo.py
+++ src/TestGraphAlgo.py
@@ -1,42 +1,41 @@
 from unittest import TestCase
+from GraphAlgo import *
 
 
 class TestGraphAlgo(TestCase):
-    def test_bfs(self):
-        self.fail()
+    def setUp(self) -> None:
+        g = GraphAlgo()
+        g.load_from_json(r"..\data\A1.json")
+        self.gAlgo = g
+
 
     def test_is_connected(self):
-        self.fail()
+        self.assertEqual(self.gAlgo.isConnected(), True)
 
-    def test_load_from_json(self):
-        self.fail()
-
-    def test_save_to_json(self):
-        self.fail()
 
     def test_nodes_edges_to_display(self):
-        self.fail()
+        gDisplay = self.gAlgo.nodesEdgesToDisplay(self.gAlgo.graph)
+        ans = gDisplay.Edges[0].weight
+        self.assertEqual(ans,1.3118716362419698)
+        ans = gDisplay.Nodes[0].pos
+        self.assertEqual(ans,"35.19589389346247,32.10152879327731,0.0")
+
+
+
 
     def test_shortest_path(self):
-        self.fail()
+        ans = self.gAlgo.shortest_path(3,11)
+        sum = 10.158987256710258
+        self.assertEqual(ans[0],sum)
 
-    def test_get_path(self):
-        self.fail()
-
-    def test_plot_graph(self):
-        self.fail()
-
-    def test_dijkstra(self):
-        self.fail()
-
-    def test_get_graph(self):
-        self.fail()
 
     def test_tsp(self):
-        self.fail()
+        ans = self.gAlgo.TSP([7, 5, 3, 12])
+        self.assertEqual(ans[1],12.98663305159289)
 
-    def test_nodes_list_to_int_lis(self):
-        self.fail()
+
 
     def test_center_point(self):
-        self.fail()
+        ans = self.gAlgo.centerPoint()
+        self.assertEqual(ans[0],8)
+        self.assertEqual(ans[1],9.925289024973141)
